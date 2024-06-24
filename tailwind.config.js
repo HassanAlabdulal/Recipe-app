c6333e1c078d622482a873b098836b62abecef24
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-06-24 10:56:30 +0300

    add: setup nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: [
+    "./app.{js,jsx,ts,tsx}",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
