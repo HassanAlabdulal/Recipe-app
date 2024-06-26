0489ab23431d61e864094e5789f5cc0b6758605d
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-06-26 16:14:05 +0300

    feat: integrate firebase to the app

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
